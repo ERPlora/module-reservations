77ad368d8931f1ba21208e47c011441eb0d3123e
Ioan Beilic <ioanbeilic@gmail.com>
2026-03-15 23:35:03 +0100

    chore: bump version to 2.0.7

--- module.py
+++ module.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 MODULE_ID = 'reservations'
 MODULE_NAME = _('Reservations')
-MODULE_VERSION = '2.0.6'
+MODULE_VERSION = '2.0.7'
 
 MENU = {
     'label': _('Reservations'),
