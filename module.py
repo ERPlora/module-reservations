ecabbab7110affcafb85f96b8511c7ece276fef4
Ioan Beilic <ioanbeilic@gmail.com>
2026-03-15 21:26:39 +0100

    chore: bump to 2.0.2 for webhook pipeline test

--- module.py
+++ module.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 MODULE_ID = 'reservations'
 MODULE_NAME = _('Reservations')
-MODULE_VERSION = '2.0.1'
+MODULE_VERSION = '2.0.2'
 
 MENU = {
     'label': _('Reservations'),
